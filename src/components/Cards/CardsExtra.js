700cd94d1b595d3056193c1c5d315125592b21da
7erhiy <7erhiy@gmail.com>
2024-05-02 22:07:23 -0300

    fix contact

--- src/components/Cards/CardsExtra.js
+++ src/components/Cards/CardsExtra.js
@@ -4,30 +4,45 @@ import Image from "next/image";
 import styles from "./CardsExtra.module.css";
 
 import iconLung from "@/assets/icons/icon-lung.svg";
+import iconHead from "@/assets/icons/icon-head.svg";
+import iconFood from "@/assets/icons/icon-food.svg";
+import iconStomach from "@/assets/icons/icon-stomach.svg";
+import iconVit from "@/assets/icons/icon-vit.svg";
+import iconCardio from "@/assets/icons/icon-cardio.svg";
+import iconReabil from "@/assets/icons/icon-psy.svg";
+import iconPsy from "@/assets/icons/icon-psy1.svg";
+import iconCheck from "@/assets/icons/icon-check.svg";
+import iconMeal from "@/assets/icons/icon-meal.svg";
+import iconTemp from "@/assets/icons/icon-temp.svg";
+import iconPhis from "@/assets/icons/icon-phis.svg";
+import iconEndo from "@/assets/icons/icon-endo.svg";
+
+
+import { useTranslations } from "next-intl";
 
 const cardsExtraData = [
   {
-    title: "Genomics",
+    title: "card0",
     descr: "asdfa asdfasdf asdfasdfasdf asdfasdfasdfadsfads saf",
     image: iconLung,
   },
-  { title: "sds2", image: iconLung },
-  { title: "sds3", image: iconLung },
-  { title: "sds4", image: iconLung },
-  { title: "sds5", image: iconLung },
-  { title: "sds6", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
-  { title: "sds7", image: iconLung },
+  { title: "card1", image: iconHead },
+  { title: "card2", image: iconMeal },
+  { title: "card3", image: iconStomach },
+  { title: "card4", image: iconVit },
+  { title: "card5", image: iconCardio },
+  { title: "card6", image: iconPsy },
+  { title: "card7", image: iconCheck },
+  { title: "card8", image: iconReabil },
+  { title: "card9", image: iconFood },
+  { title: "card10", image: iconTemp },
+  { title: "card11", image: iconPhis },
+  { title: "card12", image: iconEndo },
 ];
 
 export default function CardsExtra({ data = cardsExtraData }) {
+
+  const t = useTranslations('CardsExtra')
   const settings = {
     infinite: true,
     slidesToShow: 8,
@@ -51,7 +66,7 @@ export default function CardsExtra({ data = cardsExtraData }) {
                 <Image src={card.image} alt={card.title} width={500} height={300} className={styles.cardExtraIcon}/>
               </div>
               <div className={styles.cardExtraText}>
-                <p className={styles.cardExtraTitle}>{card.title}</p>
+                <p className={styles.cardExtraTitle}>{t(card.title)}</p>
               </div>
             </div>
           ))}
