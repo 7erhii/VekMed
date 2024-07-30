08db0276850dbcb8fe942e8b9a6b5956f20eda34
7erhiy <7erhiy@gmail.com>
2024-07-30 00:48:13 -0300

    added deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Путь к вашему проекту
+PROJECT_DIR="/root/vekmed"
+
+# Переход в директорию проекта
+cd $PROJECT_DIR
+
+# Обновление репозитория
+git pull origin main
+
+# Установка зависимостей
+npm install
+
+# Сборка проекта
+npm run build
+
+# Перезапуск приложения с использованием PM2
+pm2 restart vekmed
